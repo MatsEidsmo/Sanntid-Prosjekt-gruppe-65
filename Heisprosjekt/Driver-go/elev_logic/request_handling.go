fca09aaa6f3b1287e7c8e02267955fca1e3eebdb
nei <yooho@stud.ntnu.no>
2025-02-12 08:57:26 +0100

    latest working code

--- Heisprosjekt/Driver-go/elev_logic/request_handling.go
+++ Heisprosjekt/Driver-go/elev_logic/request_handling.go
@@ -50,50 +50,39 @@ func Clear_Floor_Requests(e *ec.Elevator) {
 	eio.SetButtonLamp(eio.BT_Cab, e.Floor, false)
 }
 
-func Choose_Dir(e *ec.Elevator) eio.MotorDirection {
+func Choose_Dir(e *ec.Elevator) {
 	switch e.Dir {
 	case eio.MD_Up:
 		if requests_above(e) {
-			return eio.MD_Up
-			// e.Dir = eio.MD_Up
-			// eio.SetMotorDirection(e.Dir)
+			e.Dir = eio.MD_Up
+			eio.SetMotorDirection(e.Dir)
 		} else if requests_below(e) {
-			return eio.MD_Down
-			// e.Dir = eio.MD_Down
-			// eio.SetMotorDirection(e.Dir)
+			e.Dir = eio.MD_Down
+			eio.SetMotorDirection(e.Dir)
 		} else {
-			return eio.MD_Stop
-			// eio.SetMotorDirection(eio.MD_Stop)
+			eio.SetMotorDirection(eio.MD_Stop)
 		}
 	case eio.MD_Down:
 		if requests_below(e) {
-			return eio.MD_Down
-			// e.Dir = eio.MD_Down
-			// eio.SetMotorDirection(e.Dir)
+			e.Dir = eio.MD_Down
+			eio.SetMotorDirection(e.Dir)
 		} else if requests_above(e) {
-			return eio.MD_Up
-			// e.Dir = eio.MD_Up
-			// eio.SetMotorDirection(e.Dir)
+			e.Dir = eio.MD_Up
+			eio.SetMotorDirection(e.Dir)
 		} else {
-			return eio.MD_Stop
-			//eio.SetMotorDirection(eio.MD_Stop)
+			eio.SetMotorDirection(eio.MD_Stop)
 		}
 	case eio.MD_Stop:
 		if requests_below(e) {
-			return eio.MD_Down
-			// e.Dir = eio.MD_Down
-			// eio.SetMotorDirection(e.Dir)
+			e.Dir = eio.MD_Down
+			eio.SetMotorDirection(e.Dir)
 		} else if requests_above(e) {
-			return eio.MD_Up
-			// e.Dir = eio.MD_Up
-			// eio.SetMotorDirection(e.Dir)
+			e.Dir = eio.MD_Up
+			eio.SetMotorDirection(e.Dir)
 		} else {
-			return eio.MD_Stop
-			//eio.SetMotorDirection(eio.MD_Stop)
+			eio.SetMotorDirection(eio.MD_Stop)
 		}
 	}
-
-	return eio.MD_Stop
 }
 
 func Stop_Here(e *ec.Elevator) bool {
